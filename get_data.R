0705a61bee0c72709007313215ce228684d743f3
Hao Ye <lhopitalified@gmail.com>
2021-06-04 17:10:17 -0400

    update to use secrets to store data URL

--- get_data.R
+++ get_data.R
@@ -1,7 +1,13 @@
 source("data_functions.R")
 
+if (file.exists("set_data_url.R"))
+{
+    source("set_data_url.R")
+}
+
+
 # grab data from google sheets
-download_raw_data()
+download_raw_data(DATA_URL)
 
 # transform data and resave
 collections_data <- process_raw_data()
